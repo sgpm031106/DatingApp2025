9384c1302f875231f4dc4a638d69637ebc8b1ade
sergioprogr@gmail.com <sergioprogr@gmail.com>
2025-09-14 20:46:17 -0600

    parcial01 - Add Angular Skeleton

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -10,18 +11,18 @@ namespace API.Controllers
     public class MembersController(AppDbContext context) : ControllerBase
     {
         [HttpGet]
-        public ActionResult<IReadOnlyList<AppUser>> GetMembers()
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
         {
-            var members = context.Users.ToList();
+            var members = await context.Users.ToListAsync();
 
             return members;
         }
 
         [HttpGet("{id}")] // https://localhost:5001/api/members/bob-id
 
-        public ActionResult<AppUser> GetMember(string id)
+        public async Task<ActionResult<AppUser>> GetMember(string id)
         {
-            var member = context.Users.Find(id);
+            var member = await context.Users.FindAsync(id);
 
             if (member == null) return NotFound();
 
